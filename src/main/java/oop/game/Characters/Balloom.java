85e6f3c433dd9d26c3ea01099ef45983156cddd9
Duongmanhvp <dinhmanhbh03@gmail.com>
2022-10-03 20:42:24 +0700

    .
    .

---
+++ src/main/java/oop/game/Characters/Balloom.java
@@ -0,0 +1,14 @@
+package oop.game.Characters;
+
+import javafx.scene.image.Image;
+
+public class Balloom extends Entity {
+
+    public Balloom(int x, int y, Image img) {
+        super(x, y, img);
+    }
+
+    public void update() {
+
+    }
+}
